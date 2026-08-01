2d9e26a2000d921b85c8da43cd95a3f4ba5cac08
vivekpatil2054 <vivekpatil2054@gmail.com>
2026-08-01 23:15:43 +0530

    Add files via upload

---
+++ program3.py
@@ -0,0 +1,111 @@
+#1
+print("hello Core2web")
+
+#2
+a = 12
+print(a)
+
+#3
+b = 1.5
+print(b)
+
+#4
+c = a + 3j
+print(c)
+
+#5
+name = "vivek patil"
+print(name)
+
+#6
+d = True
+print(d)
+
+#7
+e = 25
+print(type(e))
+
+#8
+f = 31.5
+print(type(f))
+
+#9
+g = 10
+h = 20
+i = 1.5
+sum = g+h+i
+print(sum)
+
+#10
+names = ["Vivek","Raj","Mohan","Mohit","Omraj"]
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+#11
+friends = ["goat","tiger","lion"]
+print(friends)
+
+#12
+e11 = [10,20,30,40]
+sumes =(e11[0] + e11[1] + e11[2] +e11[3])
+print(sumes)
+
+#13
+e12 = {10,20,30,30,40,40,50}
+print(e12)
+
+#14
+e13 = ["boat","college","pen","Core2web"]
+print(e13[3])
+
+#15
+student_data = {"name":"Vivek","age":22,"marks":90}
+print(student_data)
+
+#16
+student_data = {"name":"jayesh"}
+print(student_data)
+
+#17
+data = { 0:10, 1:20 , 2:25 , 3:30 , 4:40}
+print(data)
+
+sumdata = data[2] + data[3]
+print(sumdata)
+
+#18
+k = None
+print(k)
+
+#19
+name1 = 'vivek'
+name2 = "dinesh"
+name3 = """patil"""
+print(name1)
+print(name2)
+print(name3)
+
+#20
+intval = 10
+floatval = 14.4
+complexval = 2 +5j
+stringval = "vivek"
+listval = ["goat","tiger","lion"]
+tupleval = ("joker",10,20,"circle")
+dictval = {"name4":"hitman","age":39,"jerseyno":45}
+setval = {10,"umesh",20,"virat"}
+booleanval = True
+Noneval = None
+
+print(type(intval))
+print(type(floatval))
+print(type(complexval))
+print(type(stringval))
+print(type(listval))
+print(type(tupleval))
+print(type(dictval))
+print(type(setval))
+print(type(booleanval))
+print(type(Noneval))
