b0c2997229669b2f8b46cc9f944d30f4172f25a0
vivekpatil2054 <vivekpatil2054@gmail.com>
2026-08-08 14:14:24 +0530

    Add files via upload

---
+++ Python Internal.py
@@ -0,0 +1,59 @@
+#Program1
+empID = 10
+empName = "khana"
+
+print(empID)
+print(empName)
+
+empId = 20
+print(empId)
+print(empName)
+
+#Program2
+x = -1
+y = -1
+
+print(type(x))
+print(type(y))
+
+print(id(x))
+print(id(y))
+
+#Program3(Differnece between 256 and 257)
+
+a = 256
+b = 256
+
+x = 257
+y = 257
+
+print(id(a))
+print(id(b))
+
+print(id(x))
+print(id(y))
+
+#Program4
+x = 100
+y = 200
+
+print(id(x))
+print(id(y))
+
+a = 260
+b = 260
+x = 260
+
+print(id(a))
+print(id(b))
+print(id(x))
+
+#Program5
+
+a = 270
+b = 270
+
+print(id(a))
+print(id(b))
+
+
