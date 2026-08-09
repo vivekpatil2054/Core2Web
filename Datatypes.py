8a402c611700a446e11c83b3488c25208567606a
vivekpatil2054 <vivekpatil2054@gmail.com>
2026-08-09 19:15:06 +0530

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,129 @@
+#Question 1
+x = 15
+y = 4
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x**y)
+
+#Question2
+num1 = 50
+print(num1)
+
+num1 +=50
+print(num1)
+
+num1 -=50
+print(num1)
+
+num1 *=50
+print(num1)
+
+num1 /=50
+print(num1)
+
+num1 %=50
+print(num1)
+
+num1 //=50
+print(num1)
+
+#Question3
+a = 25
+b = 30
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+
+#Question4
+
+x = 10
+y = 20
+z = 30
+print(x<y and y>z)
+print(x>y and y<z)
+
+print(x<y or y<z)
+print(x>y or y<z)
+
+print(not(x==y))
+
+#Question5
+a  = 100
+b = 100
+print(a)
+print(b)
+
+print(id(a))
+print(id(b))
+
+list1 = [1,2,3]
+list2 = [1,2,3]
+print(list1)
+print(list2)
+print(list1 is list2)
+print(list1 == list2)
+print(list1 is not list2)
+
+#Question6
+list1 = ["red","blue","green","yellow"]
+print(list1)
+print("blue" in list1)
+print("purple" in list1)
+print("pink" not in list1)
+print("red" not in list1)
+
+# #Question7
+x = 12
+y = 10
+print(x)
+print(y)
+
+print(x&y)
+print(x|y)
+print(x^y)
+print(~x)
+print(x<<2)
+print(x>>2)
+
+#Question8
+intVal = 10
+floatVal = 5.5
+stringVal = "python"
+
+print(type(intVal))
+print(type(floatVal))
+print(type(stringVal))
+
+print(type(intVal + floatVal))
+print(type(intVal * floatVal))
+print(type(floatVal /intVal))
+print(type(stringVal * 2))
+
+#Question9
+first = "Hello"
+second = "World"
+
+print(first+second)
+print(first*3)
+print(second*2)
+print((first + '')*2 + second)
+
+#Question10
+a = 10
+b = 5
+c = 3
+print(a)
+print(b)
+print(c)
+print(a+b*c)
+print(a+b*c)
+print((a+b)*c)
+print(a ** b //c)
+print(a<b and b>c or a == 10)
