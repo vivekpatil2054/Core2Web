44e79439bfe24c294b06195880f93c54e139d05d
Vivek Patil <vivekpatil2054@gmail.com>
2026-08-11 18:59:53 +0530

    Add files via upload

---
+++ input and output.py
@@ -0,0 +1,48 @@
+#Program1
+# num1 = int(input("Enter the number: "))
+# print(num1)    
+
+#Program2
+# str1 = str(input("Entet the string: "))
+# print(str1)
+
+#Program3
+# float1 = float(input("Enter the float number: "))
+# print(float1)   
+
+#program4
+# num1 = int(input("Enter the number: "))
+# num2 = int(input("Enter the number: "))
+# ans = num1 + num2
+# print(ans)   
+
+#Program5
+# str1 = str(input("Enter the name: "))
+# age1 = int(input("Enter the age: "))
+# print(str1)
+# print(age1)
+
+#program6
+# float1 = float(input("Enter the first number: "))
+# float2 = float(input("Enter the second number: "))
+# float3 = float(input("Enter the third number: "))
+# ans  = (float1+float2+float3)/3
+# print(ans)
+
+#Program7
+# num1 = int(input("Enter the number: "))
+# print(type(num1))
+
+#program8
+# str1 = str(input("Enter the string: "))
+# print(type(str1))
+
+#program9
+# float1 = float(input("Enter the float number: "))
+# print(type(float1))
+
+#Progeram10
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number: "))
+ans = (num1 * num2)
+print(ans)
