38667c17f09b611806ce6ebb40c248943196f47d
Vivek Patil <vivekpatil2054@gmail.com>
2026-08-15 17:59:24 +0530

    Add files via upload

---
+++ if else practice - 1.py
@@ -0,0 +1,84 @@
+#program 1
+int x = 10
+int y = 20
+print(x+y)
+
+#Program 2
+x = input("Enter num: ")
+if(x>0):
+    print(x)
+
+#program 3
+x = int(input())
+if x>0:
+    print(x)
+    if(x>0):
+        print(x)
+
+#Program4
+x = int(input())
+
+if x>0:
+    print(x)
+
+else:
+    print(x)
+
+#program5
+x = int(input())
+
+if x>0:
+    print(x)
+
+else(x<0):
+#     print(x)
+
+#program6
+x = int(input())
+if x>0:
+    print(x)
+
+else:
+    print(x)
+
+#program7
+x = int(input())
+
+if x>0:
+    print(x)
+
+if(x==0):
+    print(x)
+
+if(x<0):
+    print(x)
+
+#program8
+if(x>0):
+    print(x)
+elif(x==0):
+    print(x)
+else:
+    print(x)    
+
+#program9
+if(x>0):
+    print(x)
+elif(x==0):
+    print(x)
+else:
+print(x)
+
+#program10
+
+num1 = 10
+num2 = 20
+
+
+sumData = num1 + num2
+subData = num2 - num1
+
+if num1 >num2:
+    print(sumData)
+else:
+    print(subData)    
