3e4e6515963e866e2fd3497858f48d2509d77964
Vivek Patil <vivekpatil2054@gmail.com>
2026-08-17 12:22:06 +0530

    Add files via upload

---
+++ Datatypes Lecture Code.py
@@ -0,0 +1,64 @@
+#Addition of two numbers
+num1 = 50
+num2 = 30
+print(num1+num2)
+print(50+30)
+print(num1 - 20)
+
+#Numeric Data type
+empId = 10
+empSal = 6.5
+print(empId)
+print(empSal)
+print(type(empId)) 
+print(type(empSal))
+eq = 2+3j
+print(eq)
+print(type(eq))
+
+#Sequential data type
+name1 = 'datta'
+name2 = "ashish"
+name3 = "Datta"
+print(name1)
+print(name2)
+print(name3)
+
+#List
+dataList = ["Ashish","Kahna","Rahul","Madhur","Kahna"]
+print(dataList)
+print(type(dataList))
+print(dataList[-3])
+print(dataList[3])
+print(dataList[1])
+
+#Tuple
+dataTuple = ("kahna",10,"Madhur",20.5)
+print(dataTuple)
+print(type(dataTuple))
+print(dataTuple[1])
+print(dataTuple[2])
+print(dataTuple[-4])
+
+#Dictionary
+player = {18:"Virat",7:"Dhoni",45:"Rohit"}
+print(player)
+print(type(player))
+print(player[7])
+
+#Set
+setData = {10,"Kahna",20,"ashish",20.5}
+print(setData)
+print(type(setData))
+
+#Boolean
+x = True
+y = False
+print(x)
+print(type(y))
+
+#None
+x = None
+print(x)
+print(type(x))
+
