f4a67393f736788d8470ff792d5d0c7cdbf72208
Vivek Patil <vivekpatil2054@gmail.com>
2026-08-17 12:22:41 +0530

    Add files via upload

---
+++ Python Internal Lecture Code.py
@@ -0,0 +1,103 @@
+#1
+empID = 10
+empName = "Kahna"
+print(empID)
+print(empName)
+
+empID = 20
+print(empID)
+print(empName)
+
+#2
+x = 10
+name = "Shashi Sir"
+data = 20.5
+flag = True
+print(type(x))
+print(type(name))
+print(type(data))
+print(type(flag))
+
+#3
+x = 10
+y = 20
+print(type(x))
+print(type(y))
+
+print(id(x))
+print(id(y))
+
+#4
+empId1 = 10
+empName1 = "Kahna"
+print(empId1)
+print(empName1)
+print(id(empId1))
+print(id(empName1))
+
+empId1 = 20
+print(empId1)
+print(empName1)
+print(id(empId1))
+print(id(empName1))
+
+#5
+x = 10
+print(id(x))
+y = 20
+print(id(y))
+x = 20
+print(id(x))
+
+#6
+a = 100
+b = 100
+print(id(a))
+print(id(b))
+a = 150
+b = 150
+print(id(a))
+print(id(b))
+
+#7
+a = 200
+b = 200
+print(id(a))
+print(id(b))
+
+#8
+a = 270
+b = 270
+print(id(a))
+print(id(b))
+
+#9
+a = 256
+b = 256
+
+x = 257
+y = 257
+print(id(a))
+print(id(b))
+
+print(id(x))
+print(id(y))
+
+#10
+x = 100
+y = 200
+print(id(x))
+print(id(y))
+
+a = 260
+b = 260
+c = 260
+print(id(a))
+print(id(b))
+print(id(c))
+
+#11List and Tuples
+mapData = {1:"kahna",10:"rahul",25:"badhe"}
+print(mapData)
+print(mapData[1])
+print(mapData[10])
